7fe2cfa7f0857c99c72b8abff86c3e30448f37c9
Anderson Astrigi <dande.astrigi@gmail.com>
2021-05-19 12:35:00 -0300

    Return session or error

--- mongosession.go
+++ mongosession.go
@@ -17,16 +17,16 @@ func newMgoSession(s *mgo.Session) *MgoSession {
 }
 
 //GetMongoSession start a new mongo session
-func GetMongoSession(msg string, databaseURI string) *MgoSession {
+func GetMongoSession(msg string, databaseURI string) (*MgoSession, error) {
 
 	mongoSession, err := mgo.Dial(databaseURI) // dial uri can be any mongodatabase
 	if err != nil {
-		log.Fatalf("[MongoDB] CreateSession: %s\n", err)
+		return nil, err
 	}
 
 	mongoSession.SetMode(mgo.Monotonic, true)
 	mongoSession.SetSocketTimeout(1 * time.Hour) //extend for long result
 
 	log.Printf("[MongoDB] session started! %s", msg)
-	return newMgoSession(mongoSession)
+	return newMgoSession(mongoSession), nil
 }
